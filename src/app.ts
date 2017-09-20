09d7d9397e433b97711aa3cba6d697f947d7a4df
Anthony Sneed <tony@tonysneed.com>
2017-09-20 16:59:20 -0500

    Add demo of set and map.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { INotifyInfo, ObservableEntity } from 'observable-entities';
+import { INotifyInfo, ObservableEntity, ObservableSet } from 'observable-entities';
 import { Subject } from 'rxjs/Subject';
 
 // Define product as observable
@@ -15,7 +15,7 @@ class Product extends ObservableEntity {
 // Create listener that logs to console when entity is updated
 const modifyListener = new Subject<INotifyInfo>();
 modifyListener.subscribe(info => {
-  console.log(`key: ${info.key} origValue: ${info.origValue} currentValue: ${info.currentValue}`)
+  console.log(`Update - key: ${info.key} origValue: ${info.origValue} currentValue: ${info.currentValue}`)
 });
 
 // Create product and add listener
@@ -27,3 +27,32 @@ product.productName = 'Chang';
 
 // Expected output:
 // key: productName origValue: Chai currentValue: Chang
+
+// Observe collection adds and deletes
+const productSet = new ObservableSet<Product>(product);
+
+// Create listeners that log to console when entities are added or removed
+const addListener = new Subject<INotifyInfo>();
+addListener.subscribe(info => {
+  console.log(`Add - ${(info.currentValue as Product).productName}`)
+});
+productSet.addListeners.push(addListener);
+
+const removeListener = new Subject<INotifyInfo>();
+removeListener.subscribe(info => {
+  console.log(`Remove - ${(info.currentValue as Product).productName}`)
+});
+productSet.removeListeners.push(removeListener);
+
+// Add entity
+const newProduct = new Product('Aniseed Syrup');
+productSet.add(newProduct);
+
+// Expected output:
+// Add - Aniseed Syrup
+
+// Remove entity
+productSet.delete(newProduct);
+
+// Expected output:
+// Remove - Aniseed Syrup
